5e0f3556b64a0456d45925b1e5f71ad5595f83a9
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-10-20 14:56:00 +0500

    ASosiy page yani home pageni shunday qilib qoldirdim

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import { Header } from "../../components";
+import "./home.css";
+export const Home = () => {
+  return (
+    <Fragment>
+      <Header title={"SHARTNOMALAR"} />
+      
+    </Fragment>
+  );
+};
