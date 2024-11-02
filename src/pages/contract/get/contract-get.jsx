856ae0ffb54808576e748440399f8bf3b5418044
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-11-02 18:01:19 +0500

    Shartnomani id orqali olib kelib pdfga jo'natadi

---
+++ src/pages/contract/get/contract-get.jsx
@@ -0,0 +1,43 @@
+import React, { Fragment, useContext } from "react";
+import "./contract-get.css";
+import { Header } from "../../../components";
+import { useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getRequest } from "../../../request";
+import { MainContext } from "../../../utils/context/context";
+
+const fetchData = async (id) => {
+  const data = await getRequest("contract/get/" + id);
+  return data;
+};
+
+export const ContractGet = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const { setShartnoma } = useContext(MainContext);
+
+  const { data, error, isLoading, refetch, isSuccess } = useQuery(
+    ["anketa", id], // Unique query key including the ID
+    () => fetchData(id), // Use a callback to pass the id
+    { enabled: !!id } // Only run the query if id exists
+  );
+
+  const handleOpen = () => {
+    setShartnoma(data?.data?.data);
+    navigate("/pdf-contract");
+  };
+
+  return (
+    <Fragment>
+      <Header title={"Shartnoma " + id} />
+      <div className="ContractGet">
+        {isLoading ? (
+          <h3>Загрузка...</h3>
+        ) : (
+          <button onClick={handleOpen}>ЧИТАТЬ PDF-ФАЙЛ</button>
+        )}
+      </div>
+    </Fragment>
+  );
+};
