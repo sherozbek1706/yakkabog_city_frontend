b4f2b32a8aa23223530a90b0df5c7205db10f00d
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-11-25 15:02:19 +0500

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,21 @@ import React, { Fragment } from "react";
 import { Header } from "../../components";
 import "./home.css";
 export const Home = () => {
+  const slider_data = [
+    {
+      url: image1,
+      title: "YAKKABOG' CITY",
+    },
+    {
+      url: image2,
+      title: "ZAMONAVIY UYLAR",
+    },
+    {
+      url: image14,
+      title: "YAKKABOG' CITY",
+    },
+  ];
+
   return (
     <Fragment>
       <Header title={"ASOSIY SAHIFA"} />
