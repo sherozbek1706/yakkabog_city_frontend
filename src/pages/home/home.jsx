68d336609be6aa7c69aeb088713c23b6488b4f83
Baxtiyorov She'rozbek <baxtiyorovsherozbek1111@gmail.com>
2024-11-28 15:04:45 +0500

    then 4 images

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -15,6 +15,10 @@ import image1 from "../../images/slider/1.jpg";
 import image2 from "../../images/slider/2.jpg";
 import image3 from "../../images/slider/3.jpg";
 import image4 from "../../images/slider/4.jpg";
+import image5 from "../../images/slider/5.jpg";
+import image6 from "../../images/slider/6.jpg";
+import image7 from "../../images/slider/7.jpg";
+import image8 from "../../images/slider/8.jpg";
 export const Home = () => {
   const slider_data = [
     {
