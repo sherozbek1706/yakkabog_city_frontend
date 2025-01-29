085d0741e228f4f078f0eacfd2d1d9fac7f97052
sherzbek1706 <baxtiyorovsherozbek1111@gmail.com>
2025-01-29 10:07:19 +0500

    Site was pending

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://192.168.100.3:5000/api/v1/";
-// export const baseURL = "http://localhost:5000/api/v1/";
+// export const baseURL = "http://192.168.100.3:5000/api/v1/";
+export const baseURL = "http://localhost:5000/api/v1/";
